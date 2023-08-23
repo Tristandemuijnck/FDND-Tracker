3ff17a890a9a9c602612d54398cb5e8d221a0300
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2023-08-24 01:02:41 +0200

    Added globals

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button>Maak notitie</button>
+
+<style>
+    button{
+        border: none;
+        background-color: var(--light-secondary);
+        padding: 0.5rem 1rem;
+        border-radius: 0.5rem;
+        color: var(--light-primary);
+    }
+</style>
