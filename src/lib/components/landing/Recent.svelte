dc347a5948cc7297316ae5f45d0948bc6fbc0e15
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2023-08-27 00:13:30 +0200

    Added Heading & Notitie styling

--- src/lib/components/landing/Recent.svelte
+++ src/lib/components/landing/Recent.svelte
@@ -2,5 +2,5 @@
     <h3>5 min geleden</h3>
     <input type="text" name="nieuweNotitie" id="">
     <p>Sprint 13</p>
-    <button>Wijzigen</button>
+    <button><img src="icons/edit.svg" alt="">Wijzigen</button>
 </section>
